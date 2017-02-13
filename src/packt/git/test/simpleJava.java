f719ee76b1d8a268a7458dfdb45660fc92b703cc
Subhojit Saha <subhojits@gmail.com>
2017-02-13 10:32:47 +0530

    Initial Commit

---
+++ src/packt/git/test/simpleJava.java
@@ -0,0 +1,11 @@
+package packt.git.test;
+
+public class simpleJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Test");
+
+	}
+
+}
