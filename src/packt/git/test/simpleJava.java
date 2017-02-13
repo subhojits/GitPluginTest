beec3fef45f9bbf3384254799bcb9b02ad604d1d
Subhojit Saha <subhojits@gmail.com>
2017-02-13 10:40:03 +0530

    bug#1234 change

--- src/packt/git/test/simpleJava.java
+++ src/packt/git/test/simpleJava.java
@@ -4,7 +4,7 @@ public class simpleJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git Test Plus 1");
+		System.out.println("Git Test Plus 1 Plus bug#1234");
 
 	}
 
