e504321f851bd03acb7585beb45823ad6b483980
Subhojit Saha <subhojits@gmail.com>
2017-02-13 10:34:31 +0530

    Commit 1

--- src/packt/git/test/simpleJava.java
+++ src/packt/git/test/simpleJava.java
@@ -4,7 +4,7 @@ public class simpleJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git Test");
+		System.out.println("Git Test Plus 1");
 
 	}
 
